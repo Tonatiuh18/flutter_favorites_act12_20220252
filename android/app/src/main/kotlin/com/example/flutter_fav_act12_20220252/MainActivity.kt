a2ae7ac5c6a82007d181274c1e2859bb7d8a9978
Leonardo Peña <a20220252@utem.edu.mx>
2024-03-21 22:07:45 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_fav_act12_20220252/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fav_act12_20220252
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fav_act12_20220252/home.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox('favorites');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: const HomePage(),
+    );
+  }
+}
